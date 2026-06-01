497675e7a9ca5cc807059108a0a831b0347e50ae
ManamiMayoki <manamimarufa@gmail.com>
2026-06-01 11:44:38 +0600

    button and function

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import numpy as np
 
-movies_list=pickle.load(open('movies.pkl','rb'))
-movies_list=movies_list['title'].values
+def recommend(movie):
+    movie_index=new_df[new_df['title']==movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
 
+    for i in movies_list:
+        st.write(new_df.iloc[i[0]].title)
+
+
+new_df=pickle.load(open('movies.pkl','rb'))
+movies_list=new_df['title'].values
+
+similarity=pickle.load(open('similarity.pkl','rb'))
 
 st.title('Movie Recommender System')
 
@@ -12,3 +23,5 @@ option=st.selectbox(
     'How would you like to be contacted?',
     movies_list
 )
+if st.button('Recommend'):
+    recommend(option)
